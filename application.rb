7ee789637efa6d9a831bf77f0275d223fb13b5d5
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-18 16:34:24 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,8 +2,10 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World my name is"
+    resp.write "Hello, World"
+    resp.write "Hello, World, my name is"
     resp.finish
   end
 
 end
+
